0abbb70e4c4de90daaf9345ee9b63db1fbd9c176
Pedro Esteves <kropinho211@outlook.com>
2024-03-01 15:23:17 -0300

    Refactor: Mudança no tipo de retorno do Controller

--- src/main/java/com/compassuol/sp/challenge/msaddress/controller/AddressController.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/controller/AddressController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.compassuol.sp.challenge.msaddress.dto.AddressResponse;
-import com.compassuol.sp.challenge.msaddress.dto.mapper.AddressMapper;
 import com.compassuol.sp.challenge.msaddress.model.Address;
 import com.compassuol.sp.challenge.msaddress.service.AddressService;
 
@@ -20,8 +18,8 @@ public class AddressController {
     private AddressService addressService;
 
     @GetMapping("/get-address-by-cep/{cep}")
-    public ResponseEntity<AddressResponse> getAddressIdByCep(@PathVariable String cep) {
+    public ResponseEntity<Address> getAddressIdByCep(@PathVariable String cep) {
         Address address = addressService.getAddressIdByCep(cep);
-        return ResponseEntity.ok(AddressMapper.toAddressResponse(address));
+        return ResponseEntity.ok(address);
     }
 }
